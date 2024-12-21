4fc68a45abcad954618342ebfd4a44106953dc7e
Ben Shamloufard <bshamloufard@berkeley.edu>
2024-12-21 01:18:27 -0800

    Initial Commit

---
+++ Saket/SaketApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaketApp.swift
+//  Saket
+//
+//  Created by Ben Shamloufard on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SaketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
